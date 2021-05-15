f20d42f103e2d9acc1ff00df3d3efb80518ce49d
Jeongmin <tdcian71@gmail.com>
2021-05-15 18:30:46 +0900

    Initial Commit

---
+++ DynamicList/DynamicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicListApp.swift
+//  DynamicList
+//
+//  Created by APPLE on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct DynamicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
