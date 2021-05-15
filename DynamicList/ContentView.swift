d256da1c7c5aed56a73019833871315caecd50a0
Jeongmin <tdcian71@gmail.com>
2021-05-15 18:41:11 +0900

    Dynamic List 제작 중

--- DynamicList/ContentView.swift
+++ DynamicList/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+struct Stock: Identifiable {
+    var id = UUID()
+    let title: String
+    
+}
+
+class StocksViewModel: ObservableObject {
+    @Published var stocks: [Stock] = [
+        Stock(title: "Apple"),
+        Stock(title: "MicroSoft"),
+        Stock(title: "Google"),
+        Stock(title: "Amazon"),
+    ]
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = StocksViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Section(header: Text("Add New Stock")) {
+                    TextField("CompanyName")
+                }
+                List {
+                    ForEach(viewModel.stocks) { stock in
+                        StockRow(title: stock.title)
+                    }
+                }
+            }
+            .navigationTitle("Stocks")
+        }
+    }
+}
+
+struct StockRow: View {
+    let title: String
+    
+    var body: some View {
+        Label(
+            title: { Text(title) },
+            icon: { Image(systemName: "chart.bar") }
+        )
     }
 }
 
